3b7908ede7bc230fd2752226a864ef277b702581
Stas Shetko <alansvits@stass-macbook-pro.local>
2018-06-06 12:19:49 +0300

    add tableview

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,6 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: - Properties
+    @IBOutlet weak var tableView: UITableView!
+    
+    //MARK: Number buttons properties
+    @IBOutlet weak var zeroButton: DesignableButton!
+    @IBOutlet weak var oneButton: DesignableButton!
+    @IBOutlet weak var twoButton: DesignableButton!
+    @IBOutlet weak var threeButton: DesignableButton!
+    @IBOutlet weak var fourButton: DesignableButton!
+    @IBOutlet weak var fiveButton: DesignableButton!
+    @IBOutlet weak var sixButton: DesignableButton!
+    @IBOutlet weak var sevenButton: DesignableButton!
+    @IBOutlet weak var eightButton: DesignableButton!
+    @IBOutlet weak var nineButton: DesignableButton!
+    
+    //MARK: Action buttons properties
+    @IBOutlet weak var clearButton: DesignableButton!
+    @IBOutlet weak var deleteButton: DesignableButton!
+    @IBOutlet weak var divisionButton: DesignableButton!
+    @IBOutlet weak var multiplicationButton: DesignableButton!
+    @IBOutlet weak var subtractionButton: DesignableButton!
+    @IBOutlet weak var additionButton: DesignableButton!
+    @IBOutlet weak var equalityButton: DesignableButton!
+    @IBOutlet weak var decimalButton: DesignableButton!
+    @IBOutlet weak var divisionBy100Button: DesignableButton!
+    
+    //MARK: - Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
